60eca429812fc4de84e1cf9fc898cf81e6bd0da8
M1stakesWereMade <superlexlive@gmail.com>
2024-05-24 17:20:30 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,12 @@
+my_list = ['borsh','cabbage','bread','potato','fish']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'salo'
+print(my_list)
+my_dict = {'borsh': 'борщ', 'cabbage': 'капуста', 'bread': 'хлеб'}
+print(my_dict)
+print(my_dict['borsh'])
+my_dict.update({'potato': 'картошка'})
+print(my_dict)
